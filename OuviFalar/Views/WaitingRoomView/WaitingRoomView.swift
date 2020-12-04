c5665dc6027ebbc3a065cb3e18a862fd15c4f744
Danilo Lira <danilo.lira01@gmail.com>
2020-12-04 17:54:00 -0300

    Adds icon stacks and some texts

--- OuviFalar/Views/WaitingRoomView/WaitingRoomView.swift
+++ OuviFalar/Views/WaitingRoomView/WaitingRoomView.swift
@@ -9,14 +9,58 @@ import SwiftUI
 
 struct WaitingRoomView: View {
     var body: some View {
-        VStack (
-            alignment: .leading,
-            spacing: 10
-        ){
-            Text("Estamos procurando...")
-            Text("Usar máscara ajuda a previnir contágio de COVID19")
+        VStack (alignment: .leading){
+            VStack(){
+                Text("Estamos procurando...")
+                    .font(.title)
+                    .bold()
+                    .padding(.top, 30)
+            }
+            
+            VStack(alignment: .center){
+                Text("Usar máscara ajuda a previnir contágio de COVID19")
+                    .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
+                    .padding(.top, 15)
+                    .frame(width: 295, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .center)
+                Spacer()
+            }
+            
+            VStack(){
+                HStack(){
+                    Image(systemName: "waveform.path.ecg")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 32, height: 32)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    Image(systemName: "staroflife")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 32, height: 32)
+                        .foregroundColor(.blue)
+                    Image(systemName: "bandage")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 32, height: 32)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    Image(systemName: "heart")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 32, height: 32)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                }
+                
+                Text("Assim que conseguirmos a resposta, iremos te notificar!")
+                    .frame(width: 240, height: 75, alignment: .center)
+                    .font(Font.system(size: 20, design: .default))
+                Spacer()
+            }
+            
+            
            
 
+            
+            
+            
         }
         
         

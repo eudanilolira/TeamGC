a128553e9a7ac55b197c18fdd331c7d7377325c6
Pedro Spínola <pedrospinola@hotmail.com>
2020-12-05 16:05:01 -0300

    Atualizando a main com a SearchView atualizada com PickerView

--- OuviFalar/Views/OnboardingView/OnboardingView.swift
+++ OuviFalar/Views/OnboardingView/OnboardingView.swift
@@ -8,88 +8,91 @@
 import SwiftUI
 
 struct OnboardView: View{
-    let color = Color(red: 0x66/0xff, green: 0x99/0xff, blue: 0xf2/0xff)
     
     var body: some View{
-        VStack(alignment: .leading){
-            VStack(alignment: .leading){
-                Text("Bem vindo ao")
-                    .font(.largeTitle)
-                    .bold()
-                Text("Ouvi Falar.")
-                    .font(.largeTitle)
-                    .bold()
-                    .foregroundColor(color)
-            }.padding()
-            .padding(.top, 40)
-            Spacer()
-            VStack(alignment: .leading, spacing: 40){
-                HStack{
-                    Image(systemName: "arrow.up.message")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 70, height: 70)
-                    VStack(alignment: .leading){
-                        Text("Mande uma mensagem")
-                            .font(.headline)
-                            .bold()
-                        Text("Compartilhe uma mensagem sobre saúde que você recebeu em alguma rede social.")
-                            .font(.subheadline)
-                            .foregroundColor(Color(.systemGray))
-                        
-                    }
-                }.frame(maxWidth: 300)
+        NavigationView{
                 
-                HStack{
-                    Image(systemName: "checkmark.shield")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 70, height: 70)
-                    
+                VStack(alignment: .leading){
                     VStack(alignment: .leading){
-                        Text("Confira a veracidade")
-                            .font(.headline)
+                        Text("Bem vindo ao")
+                            .font(.largeTitle)
                             .bold()
-                        Text("Nós fazemos a checagem se provavelmente a informação é fake news.")
-                            .font(.subheadline)
-                            .foregroundColor(Color(.systemGray))
+                        Text("Ouvi Falar.")
+                            .font(.largeTitle)
+                            .bold()
+                            .foregroundColor(UIColor.primaryColor)
+                    }.padding()
+                    .padding(.top, 40)
+                    Spacer()
+                    VStack(alignment: .leading, spacing: 40){
+                        HStack{
+                            Image(systemName: "arrow.up.message")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 70, height: 70)
+                            VStack(alignment: .leading){
+                                Text("Mande uma mensagem")
+                                    .font(.headline)
+                                    .bold()
+                                Text("Compartilhe uma mensagem sobre saúde que você recebeu em alguma rede social.")
+                                    .font(.subheadline)
+                                    .foregroundColor(Color(.systemGray))
+                                
+                            }
+                        }.frame(maxWidth: 300)
                         
+                        HStack{
+                            Image(systemName: "checkmark.shield")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 70, height: 70)
+                            
+                            VStack(alignment: .leading){
+                                Text("Confira a veracidade")
+                                    .font(.headline)
+                                    .bold()
+                                Text("Nós fazemos a checagem se provavelmente a informação é fake news.")
+                                    .font(.subheadline)
+                                    .foregroundColor(Color(.systemGray))
+                                
+                            }
+                        }.frame(width: 300)
+                        HStack{
+                            Image(systemName: "paperplane")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 70, height: 70)
+                            
+                            VStack(alignment: .leading){
+                                Text("Compartilhe a informação")
+                                    .font(.headline)
+                                    .bold()
+                                Text("Você pode compartilhar a informação seguro de que não se trata de fake news.")
+                                    .font(.subheadline)
+                                    .foregroundColor(Color(.systemGray))
+                                
+                            }
+                        }.frame(maxWidth: 300)
                     }
-                }.frame(width: 300)
-                HStack{
-                    Image(systemName: "paperplane")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 70, height: 70)
                     
-                    VStack(alignment: .leading){
-                        Text("Compartilhe a informação")
+                    Spacer()
+                    NavigationLink(destination: SearchView()){
+                        Text("Começar")
                             .font(.headline)
-                            .bold()
-                        Text("Você pode compartilhar a informação seguro de que não se trata de fake news.")
-                            .font(.subheadline)
-                            .foregroundColor(Color(.systemGray))
-                        
+                            .foregroundColor(Color(.systemBackground))
+                            .padding(.horizontal, 100.0)
+                            .padding()
+                            .background(UIColor.primaryColor)
+                            .cornerRadius(14)
+                            .padding(.bottom, 40)
                     }
-                }.frame(maxWidth: 300)
+                    
+                }
+                .background(Color.clear)
+                .navigationBarHidden(true)
             }
-            
-            Spacer()
-            Button(action: {
-                //
-            }){
-                Text("Começar")
-                    .font(.headline)
-                    .foregroundColor(Color(.systemBackground))
-                    .padding(.horizontal, 100.0)
-                    .padding()
-                    .background(color)
-                    .cornerRadius(14)
-                    .padding(.bottom, 40)
-            }
-            
-        }
-        .background(Color.clear)
+        
+        
     }
 }
 
@@ -100,3 +103,7 @@ struct OnboardView_Previews: PreviewProvider{
     }
 }
 
+extension UIColor {
+    static let primaryColor = Color(red: 0x66/0xff, green: 0x99/0xff, blue: 0xf2/0xff)
+    static let primaryUIColor = UIColor(red: 0x66/0xff, green: 0x99/0xff, blue: 0xf2/0xff, alpha: 100)
+}

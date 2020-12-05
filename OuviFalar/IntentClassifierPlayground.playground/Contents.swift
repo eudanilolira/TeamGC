72b9e45eb9f986cdca8a49eaa92a7e049da4c209
Samuel <samuelbsantosn@gmail.com>
2020-12-05 11:47:04 -0300

    Adicionar Playground criador de modelo de Intenção

---
+++ OuviFalar/IntentClassifierPlayground.playground/Contents.swift
@@ -0,0 +1,25 @@
+import Foundation
+import CreateML
+
+guard let trainingDataFileURL = Bundle.main.url(forResource: "tweets_tratados", withExtension: "json"),
+let testingDataFileURL = Bundle.main.url(forResource: "testing_tweets_tratados", withExtension: "json") else {
+    fatalError("Error! No resource files found!")
+}
+do {
+    let trainingDataTable = try MLDataTable(contentsOf: trainingDataFileURL)
+    let testingDataTable = try MLDataTable(contentsOf: testingDataFileURL)
+    let intentClassifier = try MLTextClassifier(trainingData: trainingDataTable, textColumn: "text", labelColumn: "label")
+    let trainingAccuracy = (1.0 - intentClassifier.trainingMetrics.classificationError) * 100
+    let validationAccuracy = (1.0 - intentClassifier.validationMetrics.classificationError) * 100
+    let evaluationMetrics = intentClassifier.evaluation(on: testingDataTable, textColumn: "text", labelColumn: "label")
+    let evaluationAccuracy = (1.0 - evaluationMetrics.classificationError) * 100
+    print("Training accuracy: \(trainingAccuracy)")
+    print("Validation accuracy: \(validationAccuracy)")
+    print("Evaluation accuracy: \(evaluationAccuracy)")
+    let metadata = MLModelMetadata(author: "Samuel Brasileiro",
+                                   shortDescription: "A model trained to classify the intent of the tweet", version: "1.0")
+    let modelFileURL = URL(fileURLWithPath: "/Users/samuelbrasileiro/Downloads/TweetIntentClassifier.mlmodel")
+    try intentClassifier.write(to: modelFileURL, metadata: metadata)
+} catch {
+    print(error.localizedDescription)
+}
